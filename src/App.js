fede5c676731235da6d4c7669ae6615db57cd062
Nicholas Freiter <nfreiter@mitre.org>
2020-07-01 08:58:12 -0400

    Rough sketch of some working package loading

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import TopBar from './components/TopBar';
 import JSONOutput from './components/JSONOutput';
 import ConsoleComponent from './components/ConsoleComponent';
 import CodeMirrorComponent from './components/CodeMirrorComponent';
+import tar from 'tar-stream';
+import zlib from 'zlib';
+import http from 'http';
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -20,6 +23,73 @@ const useStyles = makeStyles((theme) => ({
 
 export default function App() {
   const classes = useStyles();
+
+  let db;
+  let request = indexedDB.deleteDatabase('TESTER');
+  request.onsuccess = function (event) {
+    request = indexedDB.open('TESTER');
+    request.onerror = function (event) {
+      console.log('Failed to make db');
+    };
+    request.onsuccess = function (event) {
+      console.log('Opened db');
+    };
+    request.onupgradeneeded = function (event) {
+      console.log('Upgrading db');
+      db = event.target.result;
+      let objectStore = db.createObjectStore('resources', { keyPath: 'url' });
+      objectStore.transaction.oncomplete = function (event) {
+        console.log('Filling db');
+        http.get('http://packages.fhir.org/hl7.fhir.r4.core/4.0.1', function (res) {
+          let extract = tar.extract();
+          const resources = [];
+          extract.on('entry', function (header, stream, next) {
+            let buf = '';
+            stream.on('data', function (chunk) {
+              buf += chunk.toString();
+            });
+            stream.on('end', function () {
+              try {
+                const resource = JSON.parse(buf);
+                if (resource.kind === 'resource') {
+                  resources.push(resource);
+                }
+              } catch (e) {}
+              next();
+            });
+
+            stream.resume();
+          });
+
+          extract.on('finish', function () {
+            for (res of resources) {
+              console.log(res.id);
+            }
+
+            const transaction = db.transaction(['resources'], 'readwrite');
+            transaction.oncomplete = () => {
+              console.log('All done adding data');
+              db
+                .transaction('resources')
+                .objectStore('resources')
+                .get('http://hl7.org/fhir/StructureDefinition/Patient').onsuccess = function (event) {
+                console.log('We found patient!!!');
+                console.log(event.target.result);
+              };
+            };
+            const objectStore = transaction.objectStore('resources');
+            resources.forEach((res) => {
+              const request = objectStore.add(res);
+              request.onsuccess = () => {
+                console.log('added ' + res.id);
+              };
+            });
+          });
+          res.pipe(zlib.createGunzip()).pipe(extract);
+        });
+      };
+    };
+  };
 
   return (
     <div className="root">
