b35b3b059825d0d10b73b527caa9644ca794dee3
Citron <scitron@mitre.org>
2021-01-22 16:35:49 -0500

    changed files to .fsh

--- src/utils/ParseExamples.js
+++ src/utils/ParseExamples.js
@@ -1,5 +1,5 @@
 export async function setExampleText(example) {
-  const module = await import(`../examples/${example}.txt`);
+  const module = await import(`../examples/${example}.fsh`);
   return fetch(module.default)
     .then((response) => response.text())
     .then((data) => {
