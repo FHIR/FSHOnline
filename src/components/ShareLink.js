7cd04af92bd78d4a853aa1eb532e901c105727c6
Nick Freiter <52747882+ngfreiter@users.noreply.github.com>
2021-09-27 18:30:27 -0500

    Support sharing FSH content via Gist links (#70)
    * Support sharing FSH content via Gist links
    
    This adds support for sharing FSH content via Gists. This
    allows for longer FSH to be shared, since a gist does not have
    the same limit in size that is enforced on us by bitly. The down
    side is that authors will have to make the gist.
    
    * Refactor the dialogs to be separate
    
    Instead of one single dialog, gists are now more of a secondary
    option that is only accessible from the share dialog, or if we
    fail to generate a shareable link directly.
    
    * Add an extra newline between files
    
    * Fix the failing tests
    
    * Add a button to copy FSH to clipboard from gist modal
    
    * Refactor copying in share modal
    
    * Update modal size to be md not sm
    
    * Make the gist modal medium sized as well for consistency

--- src/components/ShareLink.js
+++ src/components/ShareLink.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { deflateSync } from 'browserify-zlib';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { makeStyles } from '@material-ui/core/styles';
-import { Link } from '@material-ui/icons';
+import { Link as LinkIcon, FileCopy } from '@material-ui/icons';
 import {
   Button,
   Dialog,
@@ -11,10 +11,13 @@ import {
   DialogContentText,
   DialogTitle,
   IconButton,
-  TextareaAutosize,
-  Tooltip
+  Tooltip,
+  Link,
+  TextField,
+  Box
 } from '@material-ui/core';
 import { generateLink } from '../utils/BitlyWorker';
+import { theme } from '../App';
 
 const useStyles = makeStyles((theme) => ({
   iconButton: {
@@ -25,19 +28,71 @@ const useStyles = makeStyles((theme) => ({
     width: '100%',
     color: theme.palette.text.primary,
     fontWeight: 'bold'
+  },
+  copyBox: {
+    backgroundColor: theme.palette.common.lightGrey,
+    borderRadius: 2,
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingLeft: '3%',
+    textOverflow: 'ellipsis'
   }
 }));
 
 export default function ShareLink(props) {
   const classes = useStyles();
   const [openShare, setOpenShare] = useState(false);
-  const [openShareError, setOpenShareError] = useState(false);
-  const [{ copied, copyButton }, setCopied] = useState({ copied: false, copyButton: 'Copy to Clipboard' });
+  const [shareError, setShareError] = useState(false);
+  const [openGist, setOpenGist] = useState(false);
+  const [openGistError, setOpenGistError] = useState(false);
+  const [copyTip, setCopyTip] = useState('Copy to Clipboard');
+  const [{ fshCopied, fshCopyButton }, setFshCopied] = useState({
+    fshCopied: false,
+    fshCopyButton: 'Copy FSH to Clipboard'
+  });
   const [link, setLink] = useState();
+  const [gistLink, setGistLink] = useState();
+  const [showCreateGistButton, setshowCreateGistButton] = useState(true);
 
-  const updateLink = (event) => {
-    const newLink = event.target.value;
-    setLink(newLink);
+  const updateGistLink = (event) => {
+    const gistId = event.target.value.match(/gist\.github\.com\/[^/]+\/(.+)/)?.[1];
+    if (gistId) {
+      setGistLink(`https://fshschool.org/FSHOnline/#/gist/${gistId}`);
+    }
+  };
+
+  const handleShareError = () => {
+    setShareError(true);
+    handleOpenGist();
+  };
+
+  const handleOpenGist = () => {
+    setOpenShare(false);
+    setOpenGist(true);
+    setGistLink('');
+    setFshCopied({ fshCopied: false, fshCopyButton: 'Copy FSH to Clipboard' });
+  };
+
+  const handleCloseGist = () => {
+    setOpenGist(false);
+    setShareError(false);
+  };
+
+  const handleGenerateGistLink = () => {
+    if (gistLink) {
+      handleCloseGist();
+      setLink(gistLink);
+      setOpenShare(true);
+      setCopyTip('Copy to Clipboard');
+      setshowCreateGistButton(false);
+    } else {
+      setOpenGistError(true);
+    }
+  };
+
+  const handleCloseGistError = () => {
+    setOpenGistError(false);
   };
 
   const handleOpenShare = async () => {
@@ -54,65 +109,98 @@ export default function ShareLink(props) {
     const longLink = `https://fshschool.org/FSHOnline/#/share/${encoded}`;
     const bitlyLink = await generateLink(longLink);
     if (bitlyLink.errorNeeded === true) {
-      handleOpenShareError();
+      handleShareError();
     } else {
       // Removes the encoded data from the end of the url, starting at index 15
       const bitlySlice = bitlyLink.link.slice(15);
       const displayLink = `https://fshschool.org/FSHOnline/#/share/${bitlySlice}`;
       setLink(displayLink);
       setOpenShare(true);
-      setCopied({ copied: false, copyButton: 'Copy to Clipboard' });
+      setCopyTip('Copy to Clipboard');
+      setshowCreateGistButton(true);
     }
   };
 
   const handleCloseShare = () => {
     setOpenShare(false);
+    setShareError(false);
   };
 
-  const handleOpenShareError = () => {
-    setOpenShareError(true);
-  };
-
-  const handleCloseShareError = () => {
-    setOpenShareError(false);
+  const handleFSHcopy = () => {
+    setFshCopied({ fshCopied: true, fshCopyButton: 'FSH Copied to Clipboard' });
   };
 
   return (
     <>
       <Tooltip title="Share FSH" placement="top" arrow>
         <IconButton name="Share FSH" className={classes.iconButton} onClick={handleOpenShare}>
-          <Link fontSize="small" style={{ transform: 'rotate(-45deg)' }} />
+          <LinkIcon fontSize="small" style={{ transform: 'rotate(-45deg)' }} />
         </IconButton>
       </Tooltip>
-      <Dialog open={openShare} onClose={handleCloseShare} aria-labelledby="form-dialog-title" maxWidth="sm" fullWidth>
+      <Dialog open={openShare} onClose={handleCloseShare} aria-labelledby="form-dialog-title" maxWidth="md" fullWidth>
         <DialogTitle id="form-dialog-title">Share</DialogTitle>
         <DialogContent>
           <DialogContentText>Use this link to share your FSH with others!</DialogContentText>
-          <TextareaAutosize
-            id="link"
-            disabled
-            label="Your Link"
-            defaultValue={link}
-            onChange={updateLink}
-            className={classes.textArea}
-          ></TextareaAutosize>
+          <Box className={classes.copyBox}>
+            <Box maxWidth="90%" textOverflow="ellipsis" overflow="hidden">
+              {link}
+            </Box>
+            <Tooltip title={copyTip} placement="top" arrow>
+              <CopyToClipboard text={link} onCopy={() => setCopyTip('Link Copied')}>
+                <IconButton style={{ borderRadius: '0%' }}>
+                  <FileCopy />
+                </IconButton>
+              </CopyToClipboard>
+            </Tooltip>
+          </Box>
         </DialogContent>
         <DialogActions>
-          <CopyToClipboard text={link} onCopy={() => setCopied({ copied: true, copyButton: 'Link Copied' })}>
-            <Button color={copied ? 'secondary' : 'primary'}>{copyButton}</Button>
-          </CopyToClipboard>
+          {showCreateGistButton && (
+            <Button onClick={handleOpenGist} color="primary" style={{ align: 'left' }}>
+              Create Link with Gist
+            </Button>
+          )}
+          <Box style={{ flex: '1 0 0', hidden: true }} />
           <Button onClick={handleCloseShare} color="primary">
             Done
           </Button>
         </DialogActions>
       </Dialog>
-      <Dialog open={openShareError} onClose={handleCloseShareError} aria-labelledby="alert-dialog-title" maxWidth="lg">
-        <DialogTitle id="alert-dialog-title">Share Error</DialogTitle>
+      <Dialog open={openGist} onClose={handleCloseGist} aria-labelledby="alert-dialog-title" maxWidth="md" fullWidth>
+        <DialogTitle id="alert-dialog-title">Share with Gist</DialogTitle>
+        {shareError && (
+          <DialogContent style={{ color: theme.palette.common.red }}>
+            Your FSH content is too long to share directly. Please use a Gist to share.
+          </DialogContent>
+        )}
         <DialogContent>
-          <DialogContentText>There was a problem sharing your FSH. Your FSH file may be too long.</DialogContentText>
+          Create a{' '}
+          <Link id="gistURL" href="https://gist.github.com/" target="_blank">
+            Gist
+          </Link>{' '}
+          and paste the URL below to generate a shareable FSH Online link.
+          <TextField id="gistURLText" label="Gist URL" fullWidth onChange={updateGistLink} />
+        </DialogContent>
+        <DialogActions style={{ bottom: 0, right: 0 }}>
+          <Button onClick={handleCloseGist} color="primary">
+            Cancel
+          </Button>
+          <Box style={{ flex: '1 0 0', hidden: true }} />
+          <CopyToClipboard text={props.shareText} onCopy={handleFSHcopy} style={{ align: 'left' }}>
+            <Button color={fshCopied ? 'secondary' : 'primary'}>{fshCopyButton}</Button>
+          </CopyToClipboard>
+          <Button color="primary" onClick={handleGenerateGistLink} style={{ align: 'left' }}>
+            Generate Link from Gist
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <Dialog open={openGistError} onClose={handleCloseGistError} aria-labelledby="alert-dialog-title" maxWidth="lg">
+        <DialogTitle id="alert-dialog-title">Error Creating Gist Link</DialogTitle>
+        <DialogContent>
+          <DialogContentText>Could not generate a link from your Gist URL. Ensure the URL is valid.</DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleCloseShareError} color="primary" autoFocus>
+          <Button onClick={handleCloseGistError} color="primary" autoFocus>
             Keep Swimming!
           </Button>
         </DialogActions>
--- src/tests/components/ShareLink.test.js
+++ src/tests/components/ShareLink.test.js
@@ -12,9 +12,15 @@ jest.mock('copy-to-clipboard', () => {
 });
 
 let container = null;
+let generateLinkSpy;
+
 beforeEach(() => {
   container = document.createElement('div');
   document.body.appendChild(container);
+  generateLinkSpy = jest
+    .spyOn(bitlyWorker, 'generateLink')
+    .mockReset()
+    .mockResolvedValue({ link: 'success', errorNeeded: false });
 });
 
 afterEach(() => {
@@ -23,32 +29,7 @@ afterEach(() => {
   container = null;
 });
 
-it('copies link to clipboard on button click', async () => {
-  const generateLinkSpy = jest
-    .spyOn(bitlyWorker, 'generateLink')
-    .mockReset()
-    .mockResolvedValue({ link: 'success', errorNeeded: false });
-
-  const { getByRole, getByText } = render(<ShareLink shareText={'Profile: A'} />, container);
-
-  await wait(() => {
-    const shareButton = getByRole('button', { name: /Share FSH/i });
-    fireEvent.click(shareButton);
-    expect(generateLinkSpy).toHaveBeenCalled();
-  });
-
-  const copyBtn = getByText('Copy to Clipboard');
-  fireEvent.click(copyBtn);
-  const linkCopiedBtn = getByText('Link Copied');
-  expect(linkCopiedBtn).toBeDefined();
-});
-
-it('generates link when share button is clicked', async () => {
-  const generateLinkSpy = jest
-    .spyOn(bitlyWorker, 'generateLink')
-    .mockReset()
-    .mockResolvedValue({ link: 'success', errorNeeded: false });
-
+it('generates direct link when sharing', async () => {
   const { getByRole } = render(<ShareLink shareText={'Profile: A'} />, container);
 
   act(() => {
@@ -60,12 +41,7 @@ it('generates link when share button is clicked', async () => {
   });
 });
 
-it('generates link with configuration when share button is clicked', async () => {
-  const generateLinkSpy = jest
-    .spyOn(bitlyWorker, 'generateLink')
-    .mockReset()
-    .mockResolvedValue({ link: 'success', errorNeeded: false });
-
+it('generates direct link with configuration when direct link button is clicked', async () => {
   const deflateSpy = jest.spyOn(Zlib, 'deflateSync').mockReset().mockReturnValue('foo');
 
   const { getByRole } = render(
@@ -83,11 +59,38 @@ it('generates link with configuration when share button is clicked', async () =>
   });
 });
 
-it('shows an error when the FSH file is too long to share', async () => {
-  const generateLinkSpy = jest
-    .spyOn(bitlyWorker, 'generateLink')
-    .mockReset()
-    .mockResolvedValue({ link: undefined, errorNeeded: true });
+it('generates gist link when generate gist link button is clicked', async () => {
+  const { getByRole, getByText } = render(<ShareLink shareText={'Profile: A'} />, container);
+
+  act(() => {
+    const shareButton = getByRole('button', { name: /Share FSH/i });
+    fireEvent.click(shareButton);
+  });
+  await wait(() => {
+    expect(generateLinkSpy).toHaveBeenCalled();
+  });
+  act(() => {
+    const gistBtn = getByText(/Create Link with Gist/i);
+    fireEvent.click(gistBtn);
+  });
+  act(() => {
+    const gistInput = document.getElementById('gistURLText');
+    fireEvent.change(gistInput, {
+      target: { value: 'https://gist.github.com/user/59c573230cd60729df8b44ab8a67b6da' }
+    });
+  });
+  act(() => {
+    const gistBtn = getByRole('button', { name: /Generate Link from Gist/i });
+    fireEvent.click(gistBtn);
+  });
+  await wait(() => {
+    const shareModal = getByText('Share');
+    expect(shareModal).toBeDefined();
+  });
+});
+
+it('routes to Gist dialog with error when the FSH file is too long to share', async () => {
+  generateLinkSpy.mockResolvedValue({ link: undefined, errorNeeded: true });
 
   const { getByRole, getByText } = render(<ShareLink shareText={'Profile: AVeryLongProfile'} />, container);
   act(() => {
@@ -95,8 +98,10 @@ it('shows an error when the FSH file is too long to share', async () => {
     fireEvent.click(shareButton);
   });
   await wait(() => {
-    const swimBtn = getByText(/Keep Swimming!/i);
-    expect(swimBtn).toBeInTheDocument();
+    const gistButton = getByText(/Generate Link from Gist/i);
+    expect(gistButton).toBeInTheDocument();
+    const errorMessage = getByText(/Your FSH content is too long to share directly/);
+    expect(errorMessage).toBeInTheDocument();
     expect(generateLinkSpy).toHaveBeenCalled();
   });
 });
