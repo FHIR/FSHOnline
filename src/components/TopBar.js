4922d37395900509851a5c23d714422d1bcf55bf
Julia Afeltra <30803904+jafeltra@users.noreply.github.com>
2021-10-14 16:21:11 -0400

    Update SUSHI and GoFSH versions (#77)
    * Update to SUSHI 2.1.1
    
    * Update to GoFSH 1.3.2
    
    * Update displayed versions

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -59,7 +59,7 @@ export default function TopBar() {
                   FSH ONLINE
                 </Typography>
                 <Typography order={2} className={classes.versionText}>
-                  Powered by SUSHI v2.0.1 and GoFSH v1.3.1
+                  Powered by SUSHI v2.1.1 and GoFSH v1.3.2
                 </Typography>
               </StylesProvider>
             </Box>
