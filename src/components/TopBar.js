f480c597b8acfab590e2816f971692a9492a448e
Julia Afeltra <jafeltra@mitre.org>
2022-10-26 10:25:37 -0400

    Update SUSHI to v2.7.0 and GoFSH to v1.6.3

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -59,7 +59,7 @@ export default function TopBar() {
                   FSH ONLINE
                 </Typography>
                 <Typography order={2} className={classes.versionText}>
-                  Powered by SUSHI v2.6.1 and GoFSH v1.6.2
+                  Powered by SUSHI v2.7.0 and GoFSH v1.6.3
                 </Typography>
               </StylesProvider>
             </Box>
